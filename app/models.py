e990a3e7c6b912390390afb55d79f4b241fbc9c5
Tahir Bin Bashir <106302623+tahirbashir754@users.noreply.github.com>
2023-07-20 23:48:59 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class tahir(models.model):
+  name = model.CharField(max-length=15)
+  contact = model.IntegerField(max-length=12)
+  email = model.EmailField()
+  
