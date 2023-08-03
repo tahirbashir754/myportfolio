d6010cf16ccf2de2f6cb3aa702e1d158eb0dbb6a
Tahir Bin Bashir <106302623+tahirbashir754@users.noreply.github.com>
2023-08-03 12:04:56 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class tahir(models.model):
-  Full Name = model.CharField(max-length=15)
-  Email Adress = model.EmailField()
+  Full Name = model.CharField(max-length=50)
+  Email Adress = model.EmailField(max-length=30)
   Mobile Number = model.IntegerField(max-length=12)
-  Email Subject = model.CharField()
-  Your Message  = model.CharField()
+  Email Subject = model.CharField(max-length=100)
+  Your Message  = model.CharField(max-length=100)
+
+ def __str__(self):
+        return self.name
   
   
